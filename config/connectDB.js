52d06fa127e61a6bd76c5fb2a00c41d71dfa7101
VENKATTARAMANAN <ramanan451@gmail.com>
2023-10-29 19:27:37 +0530

    first commit

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.DB_URL, {
+        const connection = await mongoose.connect(process.env.MONGO_CONNECT_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
